8cc64b137ca828c7cd61ec6359a88c470853b465
Denis-92 <d.mozenic@gmail.com>
2022-09-05 22:23:57 +0200

    aggiunto starter

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log('JS SCRIPT OK!');
+
+/*
+CONSEGNA
+Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
+
+// 1) GENERA E VISUALIZZA 5 NUMERI CASUALI
+
+// 2) IMPOSTA TIMER 30 SECONDI
+
+// 3) AL TERMINE DEL TIMER I NUMERI SCOMPAIONO E COMPAIONO 5 PROMPT
+
+// 4) L'INPUT DEI 5 PROMPT VIENE SALVATO E CONFRONTATO CON I NUMERI CASUALI GENERATI AL PUNTO 1
+
+// 5) L'OUTPUT VISUALIZZA UN REPORT DEI NUMERI INDOVINATI E SBAGLIATI
+
+// INGREDIENTI:
+// MATH.FLOOR, MATH.RANDOM, QUERY.SELECTOR, CREATE.ELEMENT, TIMER, AGGIUNGI CLASSE PER "DISPAY NONE", PROMPT,
+// FORSE "INDEX OF" PER CONFRONTO INPUT CON RANDOM
